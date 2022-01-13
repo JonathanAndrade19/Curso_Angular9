295e3a302ada06490dfe20b43d81132b5e9bd8d0
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-01-12 23:39:46 -0300

    creating component product-create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './views/home/home.component';
 import { RedDirective } from './directives/red.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations:  [
@@ -38,7 +39,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
