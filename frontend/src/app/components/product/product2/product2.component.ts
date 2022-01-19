654371f1135ada5c2a0924a74197ff712ecba7b4
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-01-18 23:09:55 -0300

    new component read

---
+++ frontend/src/app/components/product/product2/product2.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { Product2DataSource, Product2Item } from './product2-datasource';
+
+@Component({
+  selector: 'app-product2',
+  templateUrl: './product2.component.html',
+  styleUrls: ['./product2.component.css']
+})
+export class Product2Component implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<Product2Item>;
+  dataSource: Product2DataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new Product2DataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
