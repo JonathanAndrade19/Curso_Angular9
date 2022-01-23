d1837fe07124aa1d4f190012380d88f21e85ff2f
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-01-23 16:52:43 -0300

    creating the delete method

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,10 @@ const routes: Routes = [
 {
   path:"products/update/:id",
   component: ProductUpdateComponent
+},
+{
+  path:"products/delete/:id",
+  component: ProductDeleteComponent
 }
 ];
 
